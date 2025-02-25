1fcbf86ad6fdda9995fca25c52782692ebdcc73a
David Bajomo <112342624+daiveedb@users.noreply.github.com>
2025-02-25 13:18:51 +0100

    Initial commit

---
+++ build/CMakeFiles/MedicalImageProcessor.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MedicalImageProcessor.dir/main.cpp.o"
+  "CMakeFiles/MedicalImageProcessor.dir/main.cpp.o.d"
+  "MedicalImageProcessor"
+  "MedicalImageProcessor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MedicalImageProcessor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/MedicalImageProcessor.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for MedicalImageProcessor.
+# This may be replaced when dependencies are built.
